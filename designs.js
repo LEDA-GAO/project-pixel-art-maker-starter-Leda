df56842a7a52480d6e49080cf14ee7a304bcf591
Leda Gao <gaol18@wfu.edu>
2023-03-09 21:34:21 -0600

    Upload my work

--- designs.js
+++ designs.js
@@ -1,10 +1,41 @@
-// Select color input
-// Select size input
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid(pixCanvas) {
+    const widthValue = document.getElementById("inputWidth").value;
+    const heightValue = document.getElementById("inputHeight").value;
+    let row;
+    let rowThis;
+    let col;
+    for(let i = 0;i<heightValue;i++){
+        row = document.createElement('tr');
+        pixCanvas.appendChild(row);
+        for(let j = 0;j<widthValue;j++){
+            rowThis = pixCanvas.lastChild;
+            col = document.createElement('td');
+            rowThis.appendChild(col);
+            col.addEventListener("click", function(event){
+                const colorValue = document.getElementById("colorPicker").value;
+                event.target.style.backgroundColor = colorValue; 
+            });
+        }
+    }
 }
+
+function removeGrid(pixCanvas) {
+    const heightValue = document.getElementById("inputHeight").value;
+    let ele = pixCanvas.firstElementChild;
+    while(ele){   
+        ele.remove();
+        ele = pixCanvas.firstElementChild;
+    }
+}
+
+document.addEventListener("submit",function(event) {
+    event.preventDefault();
+    const pixCanvas = document.querySelector('#pixelCanvas');
+    removeGrid(pixCanvas);
+    makeGrid(pixCanvas);
+});
+
+
